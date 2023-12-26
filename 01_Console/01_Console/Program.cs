b701e31321d60a5ac5102d81716d4f536d1c9ae3
go2665 <go2665@outlook.com>
2023-12-26 15:11:37 +0900

    01_Console - 프로젝트 생성, 입출력 기본

---
+++ 01_Console/01_Console/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");     // 한줄을 출력하는 코드
+            Console.WriteLine("고병조입니다.");
+
+            Console.WriteLine("가가가");
+
+            Console.Write("나나나");   // 글자를 출력하는 코드
+            Console.Write("다다다");
+
+            Console.WriteLine("라라라\n마마마");
+            
+            // 변수(Variable)
+            // 변하는 숫자. 메모리에 기록해둔 값.
+
+            // 키보드 입력을 한줄 받아서 input이라는 변수에 기록하기
+            string input = Console.ReadLine();
+            Console.WriteLine(input);           // input 변수의 내용을 출력하기
+
+
+            // 실습
+            // 시작하면 이름을 물어보고 이름을 3번 출력하는 코드 만들어보기
+        }
+    }
+}
+
+// 
+
+/*
+  ㅇㄴ라ㅓ니ㅏㅇ러ㅣ
+ㅈ다ㅓ과저ㅗㄷ
+ */
+
+/// ㄴㄱ아러ㅗㅈㄷ
+/// ㅣㅏㅓㅣㄴ아ㅓㄹ
+/// ㅣㄱ자ㅓ
+/// 
