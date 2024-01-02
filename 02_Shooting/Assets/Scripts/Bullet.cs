b6c06d694f5effd7edd2ab0a926a7333a9cc4c32
go2665 <go2665@outlook.com>
2024-01-02 14:22:11 +0900

    02_Shooting - 총알 움직이기, 애니메이션 설정하기

--- 02_Shooting/Assets/Scripts/Bullet.cs
+++ 02_Shooting/Assets/Scripts/Bullet.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // 시작하자마자 계속 오른쪽으로 초속 7로 움직이게 만들기
+
+    public float moveSpeed = 7.0f;
+
+    private void Update()
+    {
+        transform.Translate(Time.deltaTime * moveSpeed * Vector2.right);    // 총 곱한 수는? 3번
+        //transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);    // 총 곱한 수는? 4번
+    }
 }
