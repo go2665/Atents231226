f8768f798095e4f4c86cfaa7756540aeadd25b95
go2665 <go2665@outlook.com>
2024-01-03 10:17:33 +0900

    02_Shooting - 프리팹, 테스트베이스

---
+++ 02_Shooting/Assets/Scripts/Test/Test_Instantiate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_Instantiate : TestBase
+{
+    protected override void OnTest1(InputAction.CallbackContext context)
+    {
+        Debug.Log("1번 눌렀다.");
+    }
+}
