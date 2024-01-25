ae1ffa693fb86acd7dffce4e5a1352c696d83781
go2665 <77224762+go2665@users.noreply.github.com>
2024-01-25 14:03:10 +0900

    03_3D_Basic - 플레이어 외형 만들기

---
+++ 03_3D_Basic/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 실습
+    // 1. 플레이어는 WS키로 전진/후진을 한다.
+    // 2. 플레이어는 AD키로 좌회전/우회전을 한다.
+    // 3. 프레이어가 움직이면(전진/후진/좌회전/우회전) Player_Move 애니메이션이 재생된다.
+    // 4. 이동 입력이 없으면 Player_Idle 애니메이션이 재생된다.
+    // 5. Player_Move 애니메이션은 팔 다리가 앞뒤로 흔들린다.
+    // 6. Player_Idle 애니메이션은 머리가 살짝 앞뒤로 까딱거린다.
+}
