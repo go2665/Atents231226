3d9c327660835c70a158e1db6dc9583e3f5d4ffa
go2665 <77224762+go2665@users.noreply.github.com>
2024-01-30 10:27:42 +0900

    03_3D_Basic - 추적 터렛 준비

---
+++ 03_3D_Basic/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    Player player;
+    public Player Player
+    {
+        get
+        {
+            if(player == null)
+                player = FindAnyObjectByType<Player>();
+            return player;
+        }
+    }
+
+    protected override void OnInitialize()
+    {
+        player = FindAnyObjectByType<Player>();
+    }
+}
--- 03_3D_Basic/Assets/Scripts/Turret/TurretTrace.cs
+++ 03_3D_Basic/Assets/Scripts/Turret/TurretTrace.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class TurretTrace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// 시야 범위
+    /// </summary>
+    public float sightRange = 10.0f;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 터렛의 머리가 돌아가는 속도
+    /// </summary>
+    public float turnSpeed = 2.0f;
+
+    /// <summary>
+    /// 터렛이 총알을 발사를 시작하는 발사각
+    /// </summary>
+    public float fireAngle = 10.0f;
+
+    private void OnDrawGizmos()
     {
-        
+        //Gizmos
+        //Handles.DrawWireDisc
     }
 }
+
+// 실습 : 추적용 터렛 만들기
+// 1. 플레이어가 터렛으로 부터 일정 거리안에 있으면 플레이어쪽으로 BarrelBody가 회전한다(플레이어를 바라보기, y축으로만 회전)
+// 2. 플레이어가 터렛의 발사각 안에 있으면 총알을 주기적으로 발사한다.
+// 3. 플레이어가 터렛의 발사각 안에 없으면 총알 발사를 정지한다.
+// 4. Gizmos를 통해 시야 범위와 발사각을 그린다.(Handles 추천)
