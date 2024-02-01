044cb68c215daaabc2e1a03fdc059073c3e2e2b8
go2665 <77224762+go2665@users.noreply.github.com>
2024-02-01 10:47:31 +0900

    03_3D_Basic - 텍스트메시 프로 추가

--- 03_3D_Basic/Assets/Scripts/Door/DoorManual.cs
+++ 03_3D_Basic/Assets/Scripts/Door/DoorManual.cs
@@ -1,9 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class DoorManual : DoorBase, IInteracable
 {
+    TextMeshPro text;   // 3D 글자(UI아님)
+
     public void Use()
     {
         Open();
     }
 }
+
+// 실습
+// 1. 일정 시간 이후에 자동으로 문이 닫히기
+// 2. 플레이어가 자신의 트리거 안에 들어오면 글자로 단축키 보여주기
