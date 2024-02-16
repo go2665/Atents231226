704b0d32ccd9c3d239ecd800b83496e7d836df9a
go2665 <77224762+go2665@users.noreply.github.com>
2024-02-16 11:40:40 +0900

    04_Tilemap - 프로젝트 생성, 에셋 추가, 룰타일 작성

---
+++ 04_TileMap/Assets/_ExternalAssets/kenney_tiny-battle/Tiled/sampleSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="198" columns="18">
+ <image source="../Tilemap/tilemap.png" trans="feae34" width="305" height="186"/>
+</tileset>
