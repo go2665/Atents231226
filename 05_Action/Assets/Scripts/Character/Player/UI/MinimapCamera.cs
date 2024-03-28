b845f6e6a8ae410123325f5b16446bc9e20b9f84
go2665 <77224762+go2665@users.noreply.github.com>
2024-03-28 10:44:15 +0900

    05_Action - 미니맵에 아이템 아이콘 표시, 미니맵 카메라 플레이어 따라다니기

--- 05_Action/Assets/Scripts/Character/Player/UI/MinimapCamera.cs
+++ 05_Action/Assets/Scripts/Character/Player/UI/MinimapCamera.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class MinimapCamera : MonoBehaviour
 {
-    // 플레이어를 따라다니는 느낌으로 작성하기
+    Vector3 offset;
+    Player player;
+    public float smoothness = 3.0f;
+
+    private void Start()
+    {
+        player = GameManager.Instance.Player;
+        transform.position = player.transform.position + Vector3.up * 30;
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, smoothness * Time.deltaTime);
+    }
 }
