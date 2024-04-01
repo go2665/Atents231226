a3c4f1796cd03a55f1011e11763b056408742802
go2665 <77224762+go2665@users.noreply.github.com>
2024-04-01 09:16:59 +0900

    05_Action - 적 HP UI 추가

---
+++ 05_Action/Assets/Scripts/Character/Enemy/EnemyHealthBar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    // 1. 적의 HP가 변경되면 이 HealthBar를 이용해 남은 HP를 표시한다.(비율로 표시)
+    // 2. 색이 정상적으로 보이도록 머티리얼 변경
+    // 3. HP는 왼쪽으로 줄어드는 듯이 보여야 한다.
+    // 4. 빌보드이어야 한다.(항상 카메라를 정면으로 바라보아야 한다)
+}
