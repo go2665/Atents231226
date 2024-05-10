769522b96f352e7db353763a355aeecf5c6a29b8
go2665 <77224762+go2665@users.noreply.github.com>
2024-05-10 12:41:30 +0900

    08_BoardGame - 주석 추가

--- 08_BoardGame/Assets/Scripts/UI/DeploymentToggle.cs
+++ 08_BoardGame/Assets/Scripts/UI/DeploymentToggle.cs
@@ -6,13 +6,29 @@ using UnityEngine.UI;
 
 public class DeploymentToggle : MonoBehaviour
 {
+    /// <summary>
+    /// 이 버튼에서 처리할 함선의 종류
+    /// </summary>
     public ShipType shipType = ShipType.None;
 
+    /// <summary>
+    /// 버튼 이미지
+    /// </summary>
     Image image;
+
+    /// <summary>
+    /// 배치 완료 표시용 게임 오브젝트
+    /// </summary>
     GameObject deployEnd;
 
+    /// <summary>
+    /// 버튼이 선택되었을 때 보일 색상
+    /// </summary>
     readonly Color selectColor = new(1, 1, 1, 0.2f);
 
+    /// <summary>
+    /// 버튼의 상태 종류
+    /// </summary>
     enum DeployState : byte
     {
         NotSelect = 0,  // 선택되지 않은 상태
@@ -20,32 +36,38 @@ public class DeploymentToggle : MonoBehaviour
         Deployed        // 배치 완료된 상태
     }
 
+    /// <summary>
+    /// 버튼의 현재 상태
+    /// </summary>
     DeployState state = DeployState.NotSelect;
 
+    /// <summary>
+    /// 버튼의 상태 확인 및 설정용 프로퍼티
+    /// </summary>
     DeployState State
     {
         get => state;
         set
         {
-            if(state != value)
+            if(state != value)  // 상태가 변경되었을 때만 처리
             {
                 state = value;
                 switch (state)
                 {
-                    case DeployState.NotSelect:
-                        image.color = Color.white;
-                        deployEnd.SetActive(false);
-                        player.UndoShipDeploy(shipType);
+                    case DeployState.NotSelect:             // 선택안됨 상태일 때
+                        image.color = Color.white;          // 색상을 원상 복귀
+                        deployEnd.SetActive(false);         // 배치 완료 표시 안보이게 하기
+                        player.UndoShipDeploy(shipType);    // 함선이 배치된 상태면 배치 취소
                         break;
-                    case DeployState.Select:
-                        image.color = selectColor;
-                        deployEnd.SetActive(false);
-                        player.SelectShipToDeploy(shipType);
-                        onSelect?.Invoke(this);
+                    case DeployState.Select:                // 선택됨 상태일 때
+                        image.color = selectColor;          // 색상을 반투명하게 만들기
+                        deployEnd.SetActive(false);         // 배치 완료 표시 안보이게 하기
+                        player.SelectShipToDeploy(shipType);    // 함선을 배치하기 위해 선택하고
+                        onSelect?.Invoke(this);             // 선택되었음을 알림
                         break;
-                    case DeployState.Deployed:
-                        image.color = selectColor;
-                        deployEnd.SetActive(true);
+                    case DeployState.Deployed:              // 배치 완료 상태일 때
+                        image.color = selectColor;          // 색상은 반투명하게 보이게 하기
+                        deployEnd.SetActive(true);          // 배치 완료 표시 보이게 하기
                         break;
                 }
             }
@@ -57,6 +79,9 @@ public class DeploymentToggle : MonoBehaviour
     /// </summary>
     public Action<DeploymentToggle> onSelect;
 
+    /// <summary>
+    /// 함선들을 가진 유저 플레이어
+    /// </summary>
     UserPlayer player;
 
     private void Awake()
@@ -70,27 +95,31 @@ public class DeploymentToggle : MonoBehaviour
 
     private void Start()
     {
-        player = GameManager.Instance.UserPlayer;
+        player = GameManager.Instance.UserPlayer;       // 플레이어 저장하기
 
-        Ship targetShip = player.GetShip(shipType);
+        Ship targetShip = player.GetShip(shipType);     // 이 버튼과 묶인 함선 찾기
         if (targetShip != null)
         {
-            targetShip.onDeploy += (isDeploy) =>
+            targetShip.onDeploy += (isDeploy) =>        // 그 함선이 배치되거나 배치해제되면 실행
             {
                 if (isDeploy)
                 {
-                    State = DeployState.Deployed;
+                    State = DeployState.Deployed;       // 함선이 배치된 상태면 버튼도 배치 상태로 변경
                 }
                 else
                 {
-                    State = DeployState.NotSelect;
+                    State = DeployState.NotSelect;      // 함선이 배치 해제된 상태면 버튼은 NotSelect로 변경
                 }
             };
         }
     }
 
+    /// <summary>
+    /// 버튼이 클릭되면 실행되는 함수
+    /// </summary>
     private void OnClick()
     {
+        // 상태별로 다른 상태로 전환되기
         switch(state)
         {
             case DeployState.NotSelect:
@@ -101,17 +130,20 @@ public class DeploymentToggle : MonoBehaviour
                 State = DeployState.NotSelect;
                 break;
             case DeployState.Deployed:
-                // 배치된 배를 배치 취소
                 State = DeployState.NotSelect;
+                // 배치된 배를 배치 취소
                 break;
         }
     }
 
+    /// <summary>
+    /// 한 종류의 배치 버튼이 눌려졌을 때 다른 버튼들을 선택 해제 상태로 만들기 위해 사용되는 함수
+    /// </summary>
     public void SetNotSelect()
     {
-        if(State != DeployState.Deployed)
+        if(State != DeployState.Deployed)       // 배치 상태면
         {
-            State = DeployState.NotSelect;
+            State = DeployState.NotSelect;      // 선택 안됨 상태로 변경
         }
     }
 
--- 08_BoardGame/Assets/Scripts/UI/ShipDeploymentPanel.cs
+++ 08_BoardGame/Assets/Scripts/UI/ShipDeploymentPanel.cs
@@ -9,13 +9,17 @@ public class ShipDeploymentPanel : MonoBehaviour
 
     private void Awake()
     {
-        toggles = GetComponentsInChildren<DeploymentToggle>();
+        toggles = GetComponentsInChildren<DeploymentToggle>();  // 모든 자식 토글버튼 찾기
         foreach (DeploymentToggle toggle in toggles)
         {
-            toggle.onSelect += UnSelectOthers;
+            toggle.onSelect += UnSelectOthers;      // 한 토글버튼이 눌려지면 다른 모든 토글버튼을 NotSelect상태로 만들기
         }
     }
 
+    /// <summary>
+    /// self를 제외한 형제 토글버튼을을 NotSelect로 만드는 함수
+    /// </summary>
+    /// <param name="self">지금 눌려진 토글 버튼(자기자신)</param>
     private void UnSelectOthers(DeploymentToggle self)
     {
         foreach(DeploymentToggle toggle in toggles)
