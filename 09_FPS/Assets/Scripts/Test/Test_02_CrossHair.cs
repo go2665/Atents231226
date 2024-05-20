802e28b229d8e7fc05069d7cb69450c8e53bf360
go2665 <77224762+go2665@users.noreply.github.com>
2024-05-20 10:18:28 +0900

    08_BoardGame - 핵심 스크립드 가져오기, 십자선 준비중

---
+++ 09_FPS/Assets/Scripts/Test/Test_02_CrossHair.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_02_CrossHair : TestBase
+{
+    protected override void OnTestLClick(InputAction.CallbackContext context)
+    {
+        base.OnTestLClick(context);
+    }
+}
---
+++ 09_FPS/Assets/Scripts/UI/CrossHair.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour
+{
+    // 십자선이 원래 위치로 복구하는 속도를 지정하기 위한 커브 필요
+    // 최대 확장 크기 지정하기
+    // 마우스 좌클릭을 할 때마다 조금씩 확장. 시간이 지나면 원래 위치로 돌아옴
+
+    public void Expend(float amount)
+    {
+
+    }
+}
